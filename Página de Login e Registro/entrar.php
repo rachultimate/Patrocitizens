a62e55e172ee41670ede2a3b84cd7561d8a9d1dc
Henrique <59806140+rachultimate@users.noreply.github.com>
2022-02-08 20:04:37 -0300

    First and Final Update
    +everything I've ever done and coded in this project

---
+++ Página de Login e Registro/entrar.php
@@ -0,0 +1,47 @@
+<?php
+
+?>
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<meta charset="UTF-8">
+<head id="Nome e Icone">
+    <title>Patrocitizens</title>
+    <link rel="stylesheet" type="text/css" href="../css/inicio.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../icon/css/all.css"
+    <link rel="apple-touch-icon" sizes="76x76" href="../Ícone/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../Ícone/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../Ícone/favicon-16x16.png">
+    <link rel="mask-icon" href="../Ícone/safari-pinned-tab.svg" color="#137511">
+    <meta name="msapplication-TileColor" content="#00a300">
+    <meta name="theme-color" content="#ffffff">
+</head>
+<body>
+<div id="cabecalho">
+    <h1 style="box-shadow: 4px 4px 4px black; background-color: forestgreen; color: white; font-family: 'Arial Black'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Patrocitizens<i style="color: lightblue;" class="fa fa-globe-americas"></i></h1>
+</div>
+
+<div id="botaocabecalho">
+    <li class="menu"><a class="nodec" href="../index.php">&nbsp;Entrar como Pedro Henrique</a></li>
+</div>
+
+<div id="botaocabecalho2">
+    <li class="menu"><a class="nodec" href="registrar.php">&nbsp;Cadastrar-se</a></li>
+</div>
+
+<div id="loginbox"> <br> <br> <br>
+    <h1>Entrar</h1>
+    <h2>Insira seu e-mail...</h2>
+    <h2>Insira sua senha...</h2><br>
+    <li>Entrar</li>
+</div>
+</body>
+</html>
+
+
---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head id="Nome e Ícone">
+    <meta charset="UTF-8">
+    <title>Patrocitizens</title>
+        <link rel="stylesheet" type="text/css" href="css/estilo2.css"
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="icon/css/all.css"
+    <link rel="apple-touch-icon" sizes="76x76" href="Ícone/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="Ícone/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="Ícone/favicon-16x16.png">
+    <link rel="mask-icon" href="Ícone/safari-pinned-tab.svg" color="#137511">
+    <meta name="msapplication-TileColor" content="#00a300">
+    <meta name="theme-color" content="#ffffff">
+    </head>
+<header>
+    <div id="cabecalho">
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Patrocitizens<i style="color: lightblue;" class="fa fa-globe-americas"></i></h1>
+    </div>
+    <div id="searchbox">
+        <li style="list-style: none; margin-top: 2px;"><i class="fa fa-search">&nbsp; Pesquise algo</li></i>
+    </div>
+
+    <div id="botaocabecalho">
+        <li class="menu"><i class="fa fa-pencil-alt"><a style="text-decoration: none; color: black" href="editarperfil.html"> Editar perfil</a></i></li>
+    </div>
+
+    <div id="botaocabecalho2">
+        <li class="menu"><i class="fa fa-running"></i><a style="text-decoration: none; color: black" href="Página%20de%20Login%20e%20Registro/entrar.php"> Sair</a></li>
+    </div>
+</header>
+    <body style="position: relative">
+        <div id="menu">
+            <nav id="menu">
+                <ul>
+                    <br>
+                    <li><a title="Seu perfil" href="seuperfil.html"><img src="https://www.history.ox.ac.uk/sites/default/files/history/images/person/unknown-person-icon-4.jpg"></a></li>
+                    <br>
+                    <li class="menu"><i class="fa fa-child"></i><a title="Seus Amigos" style="text-decoration: none; color: black" href="amigos.html"> Amigos (0)</a></li>
+                    <br>
+                    <li style="border-bottom: 1px solid black" class="menu"><i class="fa fa-address-book"></i><a title="Seus grupos" style="text-decoration: none; color: black" href="grupos.html"> Grupos (0)</a></li>
+                    <br>
+                    <li class="menu"><a title="Suas páginas" style="text-decoration: none; color: black" href="paginas.html"><i class="fa fa-clipboard-list"></i> Páginas (0)</a></li> <br> <br> <br>
+                    <li class="menu"><a title="Encontre seu par perfeito :)" style="text-decoration: none; color: black" href="lovefinder.html"><i class="fa fa-heart"></i> Love Finder</a></li> <br>
+                    <li class="menu"><a title="Venda ou compre coisas" style="text-decoration: none; color: black" href="mercado.html"><i class="fa fa-cart-plus"></i> Mercado</a></li> <br>
+                    <li class="menu"><a title="Procura emprego? Só dê um clique :)" style="text-decoration: none; color: black" href="procuroemprego.html"><i class="fa fa-clipboard-list"></i> Procuro emprego</a></li> <br> <br> <br>
+                    <li class="menu"><i title="Voltar à Página Inicial" class="fa fa-home"></i><a style="text-decoration: none; color: black" href="index.php"> Página Inicial</a></li>
+                    <br>
+                    <li class="menu"><i title="Configurações Gerais" class="fa fa-cog"></i> <a>Configurações</a></li> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+                    <li class="menu"><i title="Suporte" class="fa fa-question-circle"></i> <a>Ajuda</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div id="novopostletra">
+            <h2 style="background-color: rgba(208,250,220,1); color: forestgreen; font-size: 20px; font-family: 'Arial Black'; margin-left: 10px">Novo post</h2>
+        </div>
+
+        <div id="novopost">
+            <h1>Escreva algo...</h1>
+        </div>
+
+        <div id="feeling">
+            <h1>Como você está se sentindo hoje?</h1>
+            <ul>
+                <li class="menu"><img title="Feliz" src="https://imagensemoldes.com.br/wp-content/uploads/2018/06/Emoji-Feliz-PNG.png"></li>
+                <li class="menu"><img title="Triste" src="https://images.emojiterra.com/twitter/v12/512px/1f625.png"></li>
+                <li class="menu"><img title="Bravo" src="https://imagensemoldes.com.br/wp-content/uploads/2018/06/Emoji-Muito-Bravo-PNG.png"></li>
+                <li class="menu"><img title="Relaxado" src="https://images.emojiterra.com/google/android-pie/512px/1f60e.png" </li>
+                <li class="menu"><img title="'-'" src="https://http2.mlstatic.com/emblema-univ-emblemax-emojis-envergonhado-32mm-kit-c4-r0110-D_NQ_NP_665458-MLB29064007132_122018-F.webp"</li>
+            </ul>
+        </div>
+
+        <div id="way">
+            <h1>Onde você está hoje?</h1>
+        </div>
+
+        <div id="friendmeet">
+            <h1>Com quem você está hoje?</h1>
+        </div>
+
+        <div id="friendbox">
+            <li>Digite algo...</li>
+        </div>
+
+        <div id="friendbutton">
+            <li>Enviar</li>
+        </div>
+
+        <div id="waybox">
+            <li>Digite algo...</li>
+        </div>
+
+        <div id="notificacoes">
+            <li><i class="fa fa-bell"></i></li>
+        </div>
+
+        <div id="waybutton">
+            <li>Enviar</li>
+        </div>
+
+        <div id="enquete">
+            <li>Responda à uma enquete</li>
+        </div>
+
+        <div id="propaganda">
+            <a target="_blank" href="https://colegiobias.com" title="Colégio Bias de Priene"><img src="https://colegiobias.com/wp-content/uploads/2020/01/logo_bias_bilingue.jpeg"></a>
+        </div>
+<br>
+    <div id="feed">
+        <h2>Seu Feed</h2>
+    </div>
+
+    <div id="post">
+        <div id="inside">
+            <a style="cursor: pointer" href="seuperfil.html"><img class="fotoicone" src="https://www.facebook.com/search/async/profile_picture/?fbid=100004724461230&width=72&height=72"></a><h1 class="nome">&nbsp;Aparecida de Fátima </h1><h1 class="continuacao">&nbsp; postou uma nova foto</h1><br><h4 class="horario">Há 14h &nbsp;<i class="fa fa-clock"></i></h4>
+            <img class="fotopost" src="https://scontent.ffor10-1.fna.fbcdn.net/v/t1.0-9/94069922_1637698619730938_8795226332167405568_n.jpg?_nc_cat=105&_nc_sid=8024bb&_nc_eui2=AeGJE9XAPRDKkboDyP7DtwqsNPUFP0_thIE09QU_T-2EgTBrvSypXLUbkXcen-Io6nyQw0ofQaEAalaxfAoI9hPK&_nc_ohc=Rho-NzurBR8AX8yIAfT&_nc_ht=scontent.ffor10-1.fna&oh=b9458e21a57f4be245339ae3fbdd0e38&oe=5EC61DDD">
+        </div>
+        <div id="likebutton">
+            <li class="gostei"><i class="fa fa-thumbs-up"></i> Gostei</li>
+            <h1><a title="Pedro Ferreira, José Antônio Ferreira, Vírginia Ferreira, Mark Zuckerberg, Virgílio, Gabriel, Pedro Rosa, Santiago, Augusto Peres, Eduardo, Miguel">21 pessoas gostaram</a></h1>
+        </div>
+        <div id="commentbutton">
+            <li><i class="fa fa-comments"></i> Comentar</li>
+            <h1>5 comentários</h1>
+        </div>
+        <div id="sharebutton">
+            <li><i class="fa fa-share-square"></i>Compartilhe</li>
+            <h1><a title="Mark Zuckerberg, Pedro Ferreira, José Antônio">3 compartilhamentos</a></h1>
+        </div>
+        <div id="vercomment">
+            <li>Ver comentários</li>
+        </div>
+    </div>
+
+<br>
+    <div id="linha">
+    </div>
+    </body>
+    <footer id="rodape">
+        <h1>Copyright &copy; 2020 - by Pedro Ferreira</h1>
+    </footer>
+</html>
